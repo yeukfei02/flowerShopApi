d7d868b243f8585c4bb2569e6527b0f9817ff1ab
Donald Wu <yeukfei02@gmail.com>
2020-04-19 19:07:49 +0800

    fix-code: fix main func

--- src/main/kotlin/com/donaldwu/main/Main.kt
+++ src/main/kotlin/com/donaldwu/main/Main.kt
@@ -19,51 +19,47 @@ fun main() {
 
         path("/api") {
             // flower route
-            app.routes {
-                path("/flower") {
-                    path("/create-flower") {
-                        post {
-                            FlowerController.createFlower(it)
-                        }
+            path("/flower") {
+                path("/create-flower") {
+                    post {
+                        FlowerController.createFlower(it)
                     }
+                }
+                get {
+                    FlowerController.getAllFlower(it)
+                }
+                path(":id") {
                     get {
-                        FlowerController.getAllFlower(it)
+                        FlowerController.getFlowerById(it)
                     }
-                    path(":id") {
-                        get {
-                            FlowerController.getFlowerById(it)
-                        }
-                        patch {
-                            FlowerController.updateFlowerById(it)
-                        }
-                        delete {
-                            FlowerController.deleteFlowerById(it)
-                        }
+                    patch {
+                        FlowerController.updateFlowerById(it)
+                    }
+                    delete {
+                        FlowerController.deleteFlowerById(it)
                     }
                 }
             }
 
             // shop route
-            app.routes {
-                path("/shop") {
-                    path("/create-shop") {
-                        post {
-                            ShopController.createShop(it)
-                        }
+            path("/shop") {
+                path("/create-shop") {
+                    post {
+                        ShopController.createShop(it)
                     }
+                }
+                get {
+                    ShopController.getAllShop(it)
+                }
+                path(":id") {
                     get {
-                        ShopController.getAllShop(it)
+                        ShopController.getShopById(it)
                     }
-                    path(":id") {
-                        get {
-                            ShopController.getShopById(it)
-                        }
-                        patch {
-                            ShopController.updateShopById(it)
-                        }
-                        delete {
-                            ShopController.deleteShopById(it)
-                        }
+                    patch {
+                        ShopController.updateShopById(it)
+                    }
+                    delete {
+                        ShopController.deleteShopById(it)
                     }
                 }
             }
