d3e56b59912f0d14367823841271c615ab8bcf84
Donald Wu <yeukfei02@gmail.com>
2020-04-19 19:31:38 +0800

    fix-code: add dotenv lib

--- src/main/kotlin/com/donaldwu/common/Common.kt
+++ src/main/kotlin/com/donaldwu/common/Common.kt
@@ -2,6 +2,7 @@ package com.donaldwu.common
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import io.github.cdimascio.dotenv.dotenv
 import java.lang.reflect.Type
 
 class Common {
@@ -10,6 +11,15 @@ class Common {
             val type: Type = object : TypeToken<Map<String?, Any?>?>() {}.type
             val gson = Gson()
             return gson.fromJson(body, type)
+        }
+
+        fun getDataBase() {
+            val dotenv = dotenv()
+            val host = dotenv["HOST"]
+            val portNumber = dotenv["PORT_NUMBER"]
+            val userName = dotenv["USERNAME"]
+            val dbName = dotenv["DB_NAME"]
+            val dbPassword = dotenv["DB_PASSWORD"]
         }
     }
 }
